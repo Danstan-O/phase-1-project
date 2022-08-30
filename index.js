66a221a06e3ce8ee8b4f7edf6c6fc08dc53b2194
Danstan Oduor <danstan.oduor@student.moringaschool.com>
2022-08-30 21:21:32 +0300

    improved

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 //code
 
 $(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollX > 18){
-            $(".navBar").addClass(".zeller");
-        }else{
-            $(".navBar").removeClass(".zeller");
-        }
+
+    $('#menu').click(function(){
+        $(this).toggleClass('far-times');
+        $('header').toggleClass(toggle);
     });
 
-    // menu-navBar toggle script
-    $(".menu-btn").click(function(){
-        $(".navBar .menu").toggleClass("active");
-        $(".menu-btn i").toggleClass("active");
+    $(window).on('scroll load', function(){
+        $('#menu').removeClass('far-times');
+        $('header').toggleClass(toggle);
+
     });
 });
 
-const button = document.querySelector('#button.submit');
+        
+        
+        
+//         if(this.scrollX > 18){
+//             $(".navBar").addClass(".zeller");
+//         }else{
+//             $(".navBar").removeClass(".zeller");
+//         }
+//     });
 
-button.addEventListener('click', () => {
-    button.innerText = 'Thank You!';
+//     // menu-navBar toggle script
+//     $(".menu-btn").click(function(){
+//         $(".navBar .menu").toggleClass("active");
+//         $(".menu-btn i").toggleClass("active");
+//     });
+// });
+
+// const button = document.querySelector('#button.submit');
+
+// button.addEventListener('click', () => {
+//     button.innerText = 'Thank You!';
     
-    window.setTimeout(() => {
-      button.innerText = 'Hire me';
-    }, 5000);
-});
+//     window.setTimeout(() => {
+//       button.innerText = 'Hire me';
+//     }, 5000);
+// });
