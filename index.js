c6cc2b0b43311aa44aa5dd87cde37e79f44de667
Danstan Oduor <danstan.oduor@student.moringaschool.com>
2022-09-02 09:27:03 +0300

    project

--- index.js
+++ index.js
@@ -17,26 +17,3 @@ $(document).ready(function(){
         
         
         
-//         if(this.scrollX > 18){
-//             $(".navBar").addClass(".zeller");
-//         }else{
-//             $(".navBar").removeClass(".zeller");
-//         }
-//     });
-
-//     // menu-navBar toggle script
-//     $(".menu-btn").click(function(){
-//         $(".navBar .menu").toggleClass("active");
-//         $(".menu-btn i").toggleClass("active");
-//     });
-// });
-
-// const button = document.querySelector('#button.submit');
-
-// button.addEventListener('click', () => {
-//     button.innerText = 'Thank You!';
-    
-//     window.setTimeout(() => {
-//       button.innerText = 'Hire me';
-//     }, 5000);
-// });
